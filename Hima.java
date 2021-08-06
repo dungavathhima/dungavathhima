ff8f132334344deac635293857321e08f71a5e72
dungavathhima <82404323+dungavathhima@users.noreply.github.com>
2021-08-06 23:13:07 +0530

    Hima.java
    Program is about hello world!

--- Hima.java
+++ Hima.java
@@ -1,27 +1,10 @@
-
-#include<iostream>
-using std::cout;
-int age;
-cout<<"Enter your age:";
-cin>>age;//age of the person.
-if(age>18){
-cout<<"Your are eligible for voting";
+public class Main{
+public static void main(string[] args){
+System.out.print("Hello world!");
 }
-else if(age=18){
-cout<<"you are  eligible for voting,your age is just crossed 17 and now you are the youth";
 }
-else if(age<18){
-cout<<"you are not eligible for voting because you are still under 18 and you are a child";
-}
-else{
-cout<<"you enter the invalid data";
-}
-return 0;
-}
-
-
-     
-        
+   
+  
    
                
           
