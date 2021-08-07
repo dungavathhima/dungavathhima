82c1d454897074d656a178669439c03a7af474cf
dungavathhima <82404323+dungavathhima@users.noreply.github.com>
2021-08-07 20:36:44 +0530

    Bindu.java

---
+++ Bindu.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+public static void main (string[] args){
+int m,days,years;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number of minutes");
+m=s.nextInt();
+days=m/1440;
+System.out.println("number of days:" +days);
+years=days/365;
+System.out.println(number of years:" +yeras);
+}
+}
